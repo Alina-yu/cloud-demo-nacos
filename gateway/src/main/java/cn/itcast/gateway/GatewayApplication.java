1a88edfa604c8c3fe2772be01fb4c5057368d484
Alina-yu <2728943724@qq.com>
2023-08-01 22:47:21 +0800

    创建网关添加路由

---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date 2023/8/1 22:25
+ * @desciption:
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
